314c919533444cb3763e694fc2260ad34c60ff6b
Joao Coutinho <me@joaoubaldo.com>
2017-06-21 20:05:27 +0100

    initial import

---
+++ README.md
@@ -0,0 +1,22 @@
+# Overview
+ESP8266 DHT sensor probe that pushes readings to influxdb
+
+## Build requirements
+- PlatformIO  
+- ESP8266 board (only tested with D1 Mini)
+
+## Usage requirements
+- influxdb accessible from the wifi network where the probe connects to
+
+## Required build flags
+- WIFI_SSID - wifi ssid
+- WIFI_PASSWD - wifi password
+- INFLUXDB_URI - influxdb write endpoint url, as in `http://INFLUXDB_HOST:INFLUXDB_PORT/write?db=INFLUXDB_DB`
+- INFLUXDB_TAGS - influxdb tags to use for all readings. Use "" if none is needed. Be careful with length! Internal string buffer is 128 bytes.
+- DEEPSLEEP - Time to deep sleep (usecs). 15e6 is 15 seconds
+
+## Building
+    PLATFORMIO_BUILD_FLAGS='-DWIFI_SSID=\"MY_SSID\" -DWIFI_PASSWD=\"MY_PASSWD\" -DINFLUXDB_URI=\"http://INFLUXDB_HOST:INFLUXDB_PORT/write?db=INFLUXDB_DB\" -DINFLUXDB_TAGS=\",KEY1=VALUE,KEY2=VALUE\" -DDEEPSLEEP="15e6"' platformio run
+
+## Uploading
+    PLATFORMIO_BUILD_FLAGS='-DWIFI_SSID=\"MY_SSID\" -DWIFI_PASSWD=\"MY_PASSWD\" -DINFLUXDB_URI=\"http://INFLUXDB_HOST:INFLUXDB_PORT/write?db=INFLUXDB_DB\" -DINFLUXDB_TAGS=\",KEY1=VALUE,KEY2=VALUE\" -DDEEPSLEEP="15e6"' platformio run -t upload
---
+++ src/esp8266_dht_probe.ino
@@ -0,0 +1,64 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+
+#include "DHT.h"
+
+#define DHTPWRPIN D3
+#define DHTPIN D4     // what digital pin the DHT22 is conected to
+#define DHTTYPE DHT22   // there are multiple kinds of DHT sensors
+#define USE_SERIAL
+
+char buffer[128];
+
+DHT dht(DHTPIN, DHTTYPE);
+HTTPClient http;
+
+void setup() {
+    pinMode(DHTPWRPIN, OUTPUT);
+
+    #ifdef USE_SERIAL
+        Serial.begin(9600);
+        Serial.setTimeout(2000);
+
+        // Wait for serial to initialize.
+        while(!Serial) {}
+        Serial.println("Device Started");
+    #endif
+
+    digitalWrite(DHTPWRPIN, HIGH);
+    dht.begin();
+
+    WiFi.begin(WIFI_SSID, WIFI_PASSWD);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+    }
+    #ifdef USE_SERIAL
+        Serial.println("WiFi connected");
+    #endif
+    float h = dht.readHumidity();
+    float t = dht.readTemperature();
+    float f = dht.readTemperature(true);
+    digitalWrite(DHTPWRPIN, LOW);
+
+    if (isnan(h) || isnan(t) || isnan(f)) {
+        #ifdef USE_SERIAL
+            Serial.println("Failed to read from DHT");
+        #endif
+        ESP.deepSleep(5e6);
+    }
+
+    http.begin(INFLUXDB_URI);
+    sprintf(buffer, "humidity%s value=%d.%02d", INFLUXDB_TAGS, (int)h, (int)(h*100)%100);
+    http.POST(buffer);
+    http.end();
+    http.begin(INFLUXDB_URI);
+    sprintf(buffer, "temperature%s value=%d.%02d", INFLUXDB_TAGS, (int)t, (int)(t*100)%100);
+    http.POST(buffer);
+    http.end();
+    delay(1000);
+    ESP.deepSleep(DEEPSLEEP); // 20e6 is 20 microseconds
+}
+
+void loop() {
+}
